4e0c4918c735568cf6c60fef2956d4c9cbdde6c0
Ila Borkar <ilaborkar399@gmail.com>
2025-06-12 03:10:59 +0530

    sixth commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import "./Footer.css"
 const Footer=()=>{
     return (
-        <footer> &copy; 2025 Vivekanand College . All rights reserved </footer>
+        <footer> 
+            &copy; 2025 Vivekanand College . All rights reserved
+        <button id="chat"> <img id="image" src='https://vck.codewonders.in/images/operator.png'/></button>
+
+        </footer>
+    
     )
 }
 export default Footer 
